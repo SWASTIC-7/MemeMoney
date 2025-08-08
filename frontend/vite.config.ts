ddf9eb9b9ffaf42dadb3212e0412780fe814d50c
SWASTIC-7 <swastic_k@ee.iitr.ac.in>
2025-08-08 20:02:41 +0530

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+const eslintPlugin = require('vite-plugin-eslint');
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    process.env.NODE_ENV !== 'production' && eslintPlugin.default(), 
+  ].filter(Boolean),
 });
